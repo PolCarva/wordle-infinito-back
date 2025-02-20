7c247f9326f0a42a33b7325951284d7d39fdce93
PolCarva <pablocarvalhogimenez@gmail.com>
2025-02-19 23:50:22 -0300

    Update dictionary configurations and imports for multiple word lengths

--- dictionaries/index.js
+++ dictionaries/index.js
@@ -1,14 +1,36 @@
-const WORD_LIST = require('./lists/word-list');
-const ACCEPTED_WORDS = require('./accepted/accepted-words');
+const WORD_LIST_1 = require('./lists/word-list-1');
+const ACCEPTED_WORDS_1 = require('./accepted/accepted-words-1');
+const WORD_LIST_2 = require('./lists/word-list-2');
+const ACCEPTED_WORDS_2 = require('./accepted/accepted-words-2');
+const WORD_LIST_3 = require('./lists/word-list-3');
+const ACCEPTED_WORDS_3 = require('./accepted/accepted-words-3');
 const WORD_LIST_4 = require('./lists/word-list-4');
 const ACCEPTED_WORDS_4 = require('./accepted/accepted-words-4');
+const WORD_LIST_6 = require('./lists/word-list-6');
+const ACCEPTED_WORDS_6 = require('./accepted/accepted-words-6');
+const WORD_LIST = require('./lists/word-list');
+const ACCEPTED_WORDS = require('./accepted/accepted-words');
 
 const DICTIONARIES = {
-    3: {
-        common: WORD_LIST_4,
-        accepted: ACCEPTED_WORDS_4,
+    1: {
+        common: WORD_LIST_1,
+        accepted: ACCEPTED_WORDS_1,
         config: {
-            extraAttempts: 6,
+            extraAttempts: 9,
+        }
+    },
+    2: {
+        common: WORD_LIST_2,
+        accepted: ACCEPTED_WORDS_2,
+        config: {
+            extraAttempts: 8,
+        }
+    },
+    3: {
+        common: WORD_LIST_3,
+        accepted: ACCEPTED_WORDS_3,
+        config: {
+            extraAttempts: 7,
         }
     },
     4: {
@@ -21,6 +43,13 @@ const DICTIONARIES = {
     5: {
         common: WORD_LIST,
         accepted: ACCEPTED_WORDS,
+        config: {
+            extraAttempts: 5,
+        }
+    },
+    6: {
+        common: WORD_LIST_6,
+        accepted: ACCEPTED_WORDS_6,
         config: {
             extraAttempts: 5,
         }
